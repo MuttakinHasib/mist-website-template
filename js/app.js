51d6e5e5d7df37fd855e147eb77cfbe5973b4b40
MuttakinHasib <hasibmolla28@gmail.com>
2019-11-08 02:29:23 +0600

    added scroll top bar & fonts-cdn link

--- js/app.js
+++ js/app.js
@@ -1,82 +1,89 @@
 // Parallax Effect
 
 $(window).scroll(function() {
-  let winScroll = $(this).scrollTop();
+    var winScroll = $(this).scrollTop();
 
-  $(".cmt-top-header,.notices-top-header,.gallery-top-header").css({
-    // backgroundPositionY: "-" + Math.floor((winScroll / 1.5) * 2) + "px"
-    backgroundPositionY: Math.floor(winScroll / 10) + "px"
+    $(".cmt-top-header,.notices-top-header,.gallery-top-header").css({
+        // backgroundPositionY: "-" + Math.floor((winScroll / 1.5) * 2) + "px"
+        backgroundPositionY: Math.floor(winScroll / 10) + "px"
+    });
+    $(".top-header").css({
+        backgroundPositionY: Math.floor(winScroll / 10) + "px"
+    });
 
-  });
-  $(".top-header").css({
-    backgroundPositionY: Math.floor(winScroll / 10) + "px"
-  });
+    // Scroll Top Bar
+
+    if (winScroll > 300) {
+        $(".scroll-bar").css({
+            opacity: "1",
+            visibility: "visible",
+            pointerEvents: "all"
+        });
+        $(".scroll-bar").fadeIn("slow");
+    } else {
+        $(".scroll-bar").fadeOut("slow");
+    }
 });
 
 $(document).ready(function() {
-  $(".slider").owlCarousel({
-    items: 1,
-    loop: true,
-    dots: false,
-    nav: false,
-    autoplay: true,
-    mouseDrag: true,
-    responsive: {
-      1280: {
-        items: 1
-      },
-      600: {
-        items: 1
-      },
-      320: {
-        items: 1
-      }
-    }
-  });
-  $(".dpt-slider").owlCarousel({
-    items: 4,
-    loop: true,
-    dots: false,
-    nav: false,
-    margin: 10,
-    autoplayTimeout: 1500,
-    autoplay: true,
-    mouseDrag: true,
-    responsive: {
-      1280: {
-        items: 4
-      },
-      600: {
-        items: 3
-      },
-      320: {
-        items: 1
-      }
-    }
-  });
+    $(".slider").owlCarousel({
+        items: 1,
+        loop: true,
+        dots: false,
+        nav: false,
+        autoplay: true,
+        mouseDrag: true,
+        responsive: {
+            1280: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            320: {
+                items: 1
+            }
+        }
+    });
+    $(".dpt-slider").owlCarousel({
+        items: 4,
+        loop: true,
+        dots: false,
+        nav: false,
+        margin: 10,
+        autoplayTimeout: 1500,
+        autoplay: true,
+        mouseDrag: true,
+        responsive: {
+            1280: {
+                items: 4
+            },
+            600: {
+                items: 3
+            },
+            320: {
+                items: 1
+            }
+        }
+    });
 
-  $(".zoom").magnificPopup({
-    type: "image",
-    gallery: {
-      enabled: true
-    }
-  });
+    $(".zoom").magnificPopup({
+        type: "image",
+        gallery: {
+            enabled: true
+        }
+    });
 
-  var containerEl = document.querySelector(".gallery-item");
+    var containerEl = document.querySelector(".gallery-item");
 
-  var mixer = mixitup(containerEl);
+    var mixer = mixitup(containerEl);
 });
-
-// Job-cell data table
-
-$('#job-cell,#industry-list').dataTable();
-
 // Active gallery button
 $(document).on("click", ".control button", function() {
-  $(this)
-    .addClass("btn-gallery-active")
-    .siblings()
-    .removeClass("btn-gallery-active");
+    $(this)
+        .addClass("btn-gallery-active")
+        .siblings()
+        .removeClass("btn-gallery-active");
 });
 
 // tab system
@@ -85,15 +92,19 @@ const tabLink = document.querySelectorAll(".tab-link");
 const tabContent = document.querySelectorAll(".tab-content");
 
 tabLink.forEach(item => {
-  item.addEventListener(function(e) {
-    e.preventDefault();
-  });
+    item.addEventListener(function(e) {
+        e.preventDefault();
+    });
 });
 
 function showPanel(index) {
-  tabContent.forEach(item => {
-    item.style.display = "none";
-  });
+    tabContent.forEach(item => {
+        item.style.display = "none";
+    });
 
-  tabContent[index].style.display = "block";
+    tabContent[index].style.display = "block";
 }
+
+// Job-cell data table
+
+$("#job-cell,#industry-list").dataTable();
