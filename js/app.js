fcd74197c1f73b5b72ce7862ebf8086768cffdc1
MuttakinHasib <hasibmolla28@gmail.com>
2019-11-07 22:19:22 +0600

    added contact, job-cell, industry-list pages

--- js/app.js
+++ js/app.js
@@ -1,79 +1,93 @@
-$(document).ready(function () {
-    $(".slider").owlCarousel({
-        items: 1,
-        loop: true,
-        dots: false,
-        nav: false,
-        autoplay: true,
-        mouseDrag: true,
-        responsive: {
-            1280: {
-                items: 1
-            },
-            600: {
-                items: 1
-            },
-            320: {
-                items: 1
-            }
-        }
-    });
-    $(".dpt-slider").owlCarousel({
-        items: 4,
-        loop: true,
-        dots: false,
-        nav: false,
-        margin: 10,
-        autoplayTimeout: 1500,
-        autoplay: true,
-        mouseDrag: true,
-        responsive: {
-            1280: {
-                items: 4
-            },
-            600: {
-                items: 3
-            },
-            320: {
-                items: 1
-            }
-        }
-    });
+// Parallax Effect
 
-    $('.zoom').magnificPopup({
+$(window).scroll(function() {
+  let winScroll = $(this).scrollTop();
 
-        type: 'image',
-        gallery: {
-            enabled: true
-        }
-    });
+  $(".cmt-top-header,.notices-top-header,.gallery-top-header").css({
+    backgroundPositionY: "-" + Math.floor((winScroll / 1.5) * 2) + "px"
+  });
+  $(".top-header").css({
+    backgroundPositionY: Math.floor(winScroll / 10) + "px"
+  });
+});
 
-    var containerEl = document.querySelector('.gallery-item');
+$(document).ready(function() {
+  $(".slider").owlCarousel({
+    items: 1,
+    loop: true,
+    dots: false,
+    nav: false,
+    autoplay: true,
+    mouseDrag: true,
+    responsive: {
+      1280: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      320: {
+        items: 1
+      }
+    }
+  });
+  $(".dpt-slider").owlCarousel({
+    items: 4,
+    loop: true,
+    dots: false,
+    nav: false,
+    margin: 10,
+    autoplayTimeout: 1500,
+    autoplay: true,
+    mouseDrag: true,
+    responsive: {
+      1280: {
+        items: 4
+      },
+      600: {
+        items: 3
+      },
+      320: {
+        items: 1
+      }
+    }
+  });
 
-    var mixer = mixitup(containerEl);
+  $(".zoom").magnificPopup({
+    type: "image",
+    gallery: {
+      enabled: true
+    }
+  });
+
+  var containerEl = document.querySelector(".gallery-item");
+
+  var mixer = mixitup(containerEl);
 });
 
 // Active gallery button
-$(document).on('click', '.control button', function () {
-    $(this).addClass('btn-gallery-active').siblings().removeClass('btn-gallery-active')
+$(document).on("click", ".control button", function() {
+  $(this)
+    .addClass("btn-gallery-active")
+    .siblings()
+    .removeClass("btn-gallery-active");
 });
 
 // tab system
 
-const tabLink = document.querySelectorAll('.tab-link');
-const tabContent = document.querySelectorAll('.tab-content');
+const tabLink = document.querySelectorAll(".tab-link");
+const tabContent = document.querySelectorAll(".tab-content");
 
 tabLink.forEach(item => {
-    item.addEventListener(function (e) {
-        e.preventDefault();
-    });
+  item.addEventListener(function(e) {
+    e.preventDefault();
+  });
 });
 
 function showPanel(index) {
+  tabContent.forEach(item => {
+    item.style.display = "none";
+  });
 
-    tabContent.forEach(item => {
-        item.style.display = 'none';
-    });
-
-    tabContent[index].style.display = 'block';
+  tabContent[index].style.display = "block";
 }
