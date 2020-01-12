70b2077d2b0882cf1f3661c3e14cbbd0f7c1c530
MuttakinHasib <hasibmolla28@gmail.com>
2020-01-12 23:18:28 +0600

    up to date

--- js/app.js
+++ js/app.js
@@ -1,105 +1,110 @@
 // Parallax Effect
 
 $(window).scroll(function() {
-    var winScroll = $(this).scrollTop();
+  var winScroll = $(this).scrollTop();
 
-    // $(".cmt-top-header,.notices-top-header,.gallery-top-header").css({
-    //     // backgroundPositionY: "-" + Math.floor((winScroll / 1.5) * 2) + "px"
-    //     backgroundPositionY: Math.floor(winScroll / 10) + "px"
-    // });
-    // $(".top-header").css({
-    //     backgroundPositionY: Math.floor(winScroll / 10) + "px"
-    // });
-
-    // Scroll Top Bar
-
-    if (winScroll >= 300) {
-        $(".scroll-bar").css({
-            opacity: "1",
-            visibility: "visible",
-            pointerEvents: "all"
-        });
-
-        $(".scroll-bar").fadeIn("slow");
-    } else {
-        $(".scroll-bar").fadeOut("slow");
-    }
-
+  // $(".cmt-top-header,.notices-top-header,.gallery-top-header").css({
+  //     // backgroundPositionY: "-" + Math.floor((winScroll / 1.5) * 2) + "px"
+  //     backgroundPositionY: Math.floor(winScroll / 10) + "px"
+  // });
+  // $(".top-header").css({
+  //     backgroundPositionY: Math.floor(winScroll / 10) + "px"
+  // });
 });
 
 $(document).ready(function() {
-    // smooth scroll
-    let scrollLink = $(".scroll-top");
-    scrollLink.click(function(e) {
-        e.preventDefault();
-        $("body,html").animate(
-            {
-                scrollTop: $(this.hash).offset().top - 100
-            },
-            1000
-        );
-    });
+  // Go top
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 300) {
+      $(".scroll-bar").fadeIn();
+    } else {
+      $(".scroll-bar").fadeOut();
+    }
+  });
 
-    // Slider
+  // smooth scroll
 
-    $(".slider").owlCarousel({
-        items: 1,
-        loop: true,
-        dots: false,
-        nav: false,
-        autoplay: true,
-        mouseDrag: true,
-        responsive: {
-            1280: {
-                items: 1
-            },
-            600: {
-                items: 1
-            },
-            320: {
-                items: 1
-            }
-        }
-    });
-    $(".dpt-slider").owlCarousel({
-        items: 4,
-        loop: true,
-        dots: false,
-        nav: false,
-        margin: 10,
-        autoplayTimeout: 1500,
-        autoplay: true,
-        mouseDrag: true,
-        responsive: {
-            1280: {
-                items: 4
-            },
-            600: {
-                items: 3
-            },
-            320: {
-                items: 1
-            }
-        }
-    });
-    // Popup Img
-    $(".zoom").magnificPopup({
-        type: "image",
-        gallery: {
-            enabled: true
-        }
-    });
+  $(".scroll-bar").on("click", function(e) {
+    $("html, body").animate(
+      {
+        scrollTop: 0
+      },
+      600
+    );
+    return false;
+  });
 
-    var containerEl = document.querySelector(".gallery-item");
+  //   let scrollLink = $(".scroll-top");
+  //   scrollLink.click(function(e) {
+  //     e.preventDefault();
+  //     $("body,html").animate(
+  //       {
+  //         scrollTop: $(this.hash).offset().top - 100
+  //       },
+  //       1000
+  //     );
+  //   });
 
-    var mixer = mixitup(containerEl);
+  // Slider
+
+  $(".slider").owlCarousel({
+    items: 1,
+    loop: true,
+    dots: false,
+    nav: false,
+    autoplay: true,
+    mouseDrag: true,
+    responsive: {
+      1280: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      320: {
+        items: 1
+      }
+    }
+  });
+  $(".dpt-slider").owlCarousel({
+    items: 4,
+    loop: true,
+    dots: false,
+    nav: false,
+    margin: 10,
+    autoplayTimeout: 1500,
+    autoplay: true,
+    mouseDrag: true,
+    responsive: {
+      1280: {
+        items: 4
+      },
+      600: {
+        items: 3
+      },
+      320: {
+        items: 1
+      }
+    }
+  });
+  // Popup Img
+  $(".zoom").magnificPopup({
+    type: "image",
+    gallery: {
+      enabled: true
+    }
+  });
+
+  var containerEl = document.querySelector(".gallery-item");
+
+  var mixer = mixitup(containerEl);
 });
 // Active gallery button
 $(document).on("click", ".control button", function() {
-    $(this)
-        .addClass("btn-gallery-active")
-        .siblings()
-        .removeClass("btn-gallery-active");
+  $(this)
+    .addClass("btn-gallery-active")
+    .siblings()
+    .removeClass("btn-gallery-active");
 });
 
 // tab system
